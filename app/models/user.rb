ef35c39b175beb4f5a96f51bcd971dc38e078fe6
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-30 15:18:49 -0500

    WIP: Bcrypt issues - can't create user from form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :date_of_birth, :email, :password, :website
+  attr_accessible :date_of_birth, :email, :password, :password_confirmation, :website
+
+  validates :email, :presence =>true,
+                    :uniqueness=>true
+  # validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
+
+  validates :password, :presence =>true, length: { minimum: 6 }
+  validates_confirmation_of :password
+  validates :password_confirmation, presence: true
+
+  has_secure_password
 end
--- db/migrate/20131030191948_create_users.rb
+++ db/migrate/20131030191948_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.date :date_of_birth
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.string :website
 
       t.timestamps
