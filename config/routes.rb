b26a1ab6053654e6fc585891a4a7a587db5ee5e1
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 08:55:21 -0500

    fixed according to Abi's comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ StackOverflow::Application.routes.draw do
 
   get '/users' => 'users#index'
   post '/users' => 'users#create'
-  get '/users/new' => 'users#new'
-  get '/users/id/edit' => 'users#edit'
-  get '/users/id' => 'users#show'
-  put '/users/id' => 'users#update'
-  delete '/users/id' => 'users#destroy'
+  get '/users/new' => 'users#new', as: "new_user"
+  get '/users/:id/edit' => 'users#edit', as: "edit_user"
+  get '/users/:id' => 'users#show', as: "user"
+  put '/users/:id' => 'users#update'
+  delete '/users/:id' => 'users#destroy'
 
   resources :questions do
     resources :answers
