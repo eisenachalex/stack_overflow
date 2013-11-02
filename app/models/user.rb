6dada75adbf4cd9539aaf308fa28c89254aa6579
Annabel <Annabel@Dunstone.local>
2013-11-01 21:12:37 -0500

    Users can edit their profile page, upload a different image. CSS changes on profile page and index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
 class User < ActiveRecord::Base
-  attr_accessible :date_of_birth, :email, :password, :password_confirmation, :website, :photo
+  attr_accessible :date_of_birth, :email, :password, :password_confirmation, :website, :photo, :current_city, :interests, :name
 
   validates :email, :presence =>true,
                     :uniqueness=>true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
-  validates :password, :presence =>true
-  validates :password, length: { minimum: 6 , message: "Password needs to be at least 6 characters" }
-  validates_confirmation_of :password
-  validates :password_confirmation, presence: true
+
+  validates :password, :presence =>true, on: :create
+  validates :password, length: { minimum: 6 , message: "Password needs to be at least 6 characters" }, on: :create
+  validates_confirmation_of :password, on: :create
+  validates :password_confirmation, presence: true, on: :create 
+
 
   has_many :questions
   has_many :answers
   has_many :comments
   has_many :badges
+
   has_one :photo
 
   has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
     question = Faker::Lorem.paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 3)
     title = (Faker::Company.catch_phrase) + "?"
     question_answer = Question.create(user_id: user.id, q_content: question, q_title: title, votes: rand(1..500))
-    2.times do
+    rand(1..5).times do
       tag_name = Faker::Company.bs
       tag_names = tag_name.scan(/(\w+)/)
       tag_names.each do |tag|
         question_answer.tags << Tag.where(t_content: tag[0]).first_or_create
       end
     end
-    5.times do
+    rand(2..12).times do
       answer = Faker::Lorem.paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 3)
       Answer.create(question_id: question_answer.id, user_id: user.id, a_content: answer, votes: rand(1..3000))
     end
